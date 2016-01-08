8bb8609b448fe0c98745b1fd03769adf45933911
Igor <kutscherenko.igor@gmail.com>
2016-01-08 23:26:43 +0000

    Posts created

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=repsblog_stg2php',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root1989%',
     'charset' => 'utf8',
 ];
